a5a927599652e7a4924371bcfb6047a5f2b79907
a3bela <abelsintayehu626@gmail.com>
2024-06-26 23:15:17 +0300

    updated

--- models/state.py
+++ models/state.py
@@ -1,36 +1,9 @@
+
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-        __tablename__: name for db table
-        cities: relation with city
-    """
-    __tablename__ = 'states'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete',
-                              backref='state')
-    else:
-        @property
-        def cities(self):
-            """Returns the list of City instances with
-            state_id equals to the current State.id
-            city_list: list of cities from a state
-            """
-            city_list = []
-
-            for key, value in models.storage.all(State):
-                    if value.state_id == self.id:
-                        city_list[key] = value
-            return city_list
+class State(BaseModel):
+    """ State class """
+    name = ""
--- models/user.py
+++ models/user.py
@@ -1,33 +1,11 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.review import Review
-from models.place import Place
+"""This module defines a class User"""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-        places: relationship to place
-        reviews: relationship to review
-    """
-
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship('Place', cascade='all, delete',
-                          backref='user')
-    reviews = relationship('Review', cascade='all, delete',
-                           backref='user')
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
