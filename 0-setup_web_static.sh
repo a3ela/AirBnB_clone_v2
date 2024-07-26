d4f6db722519e8d99eb054bd054b1b9905deb0e4
a3bela <abelsintayehu626@gmail.com>
2024-07-26 13:35:30 +0300

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
+# Sets up my web servers for the deployment of web_static
+sudo apt-get -y update
 sudo apt-get -y install nginx
 
 mkdir -p /data/
@@ -10,22 +10,21 @@ mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 
 touch /data/web_static/releases/test/index.html
-
 echo "<html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | tee /data/web_static/releases/test/index.html > /dev/null
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R "$USER": /data/
+chown -R ubuntu: /data/
 
-sudo nginx_config="/etc/nginx/sites-available/default"
+nginx_config="/etc/nginx/sites-available/default"
 if [ -e "$nginx_config" ]; then
-		sudo sed -i '/server_name_;/a \\n   location /hbnb_static/ {\n      alias /data/web_static/current/;\n   }\n' "$nginx_config"
+    sudo sed -i '/server_name _;/a \\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }\n' "$nginx_config"
 fi
 
-sudo service nginx restart
+service nginx restart
