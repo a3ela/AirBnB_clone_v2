6e5d4831bea437fd823c423ced41fbc56e79cbca
a3ela <abelsintayehu626@gmail.com>
2024-07-29 12:19:57 +0300

    fixed indent

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,14 @@ Otherwise, it should return None
 from fabric.api import local
 import time
 
+
 def do_pack():
-        """generates a .tgz archive"""
-        try:
-            local('mkdir -p versions')
-            local('tar -cvzf versions/web_static_{:s}.tgz web_static/'.
-            format(time.strftime('%Y%m%d%H%M%S')))
-            return 'versions/web_static_{:s}.tgz'.\
-            format(time.strftime('%Y%m%d%H%M%S'))
-        except BaseException:
-            return None
+    """generates a .tgz archive"""
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/web_static_{:s}.tgz web_static/'.
+              format(time.strftime('%Y%m%d%H%M%S')))
+        return 'versions/web_static_{:s}.tgz'.\
+               format(time.strftime('%Y%m%d%H%M%S'))
+    except BaseException:
+        return None
