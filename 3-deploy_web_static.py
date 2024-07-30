a1e01adcaef2db2e1a7fab702d3895a2012ebd46
a3ela <abelsintayehu626@gmail.com>
2024-07-30 07:59:46 +0300

    updated

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,80 +1,57 @@
 #!/usr/bin/python3
-
-"""Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers,
-using the function do_deploy
 """
-from fabric.api import *
-from fabric.operations import run, put
-import os
-import time
-
-
+Fabric script methods:
+    do_pack: packs web_static/ files into .tgz archive
+    do_deploy: deploys archive to webservers
+    deploy: do_packs && do_deploys
+Usage:
+    fab -f 3-deploy_web_static.py deploy -i my_ssh_private_key -u ubuntu
+"""
+from fabric.api import local, env, put, run
+from time import strftime
+import os.path
 env.hosts = ['54.237.48.16', '18.234.145.133']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
-    """generates a .tgz archive"""
+    """generate .tgz archive of web_static/ folder"""
+    timenow = strftime("%Y%M%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return "versions/web_static_{:s}.tgz".\
-            format(time.strftime("%Y%m%d%H%M%S"))
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
     except BaseException:
         return None
 
 
 def do_deploy(archive_path):
-    """""deploy you codebase"""
-    t_str = time.strftime('%Y%m%d%H%M%S')
-    f_name = archive_path.split('/')[-1]
-    f_noext = f_name.split('.')[0]
-
-    if not os.path.exists(archive_path):
+    """
+    Deploy archive to web server
+    """
+    if os.path.isfile(archive_path) is False:
         return False
     try:
-        # upload the archive to tmp of the wen server
-        put(archive_path, '/tmp/')
-
-        # create a file with the time stamp
-        run('sudo mkdir -p /data/web_static/releases/{}/'.
-            format(f_noext))
-
-        # uncomprestion
-        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-            format(f_name, f_noext))
-
-        # delete the archive
-        run('sudo rm /tmp/{}'.format(f_name))
-
-        # move contents into web_static
-        run('sudo mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}'.format(f_noext, f_noext))
-
-        # remove irrelevant web_static dir
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'.
-            format(f_noext))
-
-        # delete the symobolic link from the web server
-        run('sudo rm -rf /data/web_static/current')
-
-        # create a new stybolic link
-        run('sudo ln -s /data/web_static/releases/{}/ \
-                /data/web_static/current'.format(f_noext))
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        return True
     except BaseException:
         return False
 
-    return True
-
 
 def deploy():
-    """
-    creates and distributes an archive to my web servers
-    """
-    created_archive = do_pack()
-    if not created_archive:
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-    return do_deploy(created_archive)
+    success = do_deploy(archive_path)
+    return success
