5100d9b9cb3e279eaea1e5907baca975a5b4cbf3
a3ela <abelsintayehu626@gmail.com>
2024-07-31 00:19:58 +0300

    fixed concatination issue

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,11 +5,10 @@ Usage:
     fab -f 100-clean_web_static.py do=clean:number=2
     optional: -i sshkey -u ubuntu
 """
-import fabric.api import *
-import os
+from fabric.api import local, run, env, sudo
 
-env.hosts = ['54.237.48.16', '18.234.145.133']
 env.user = "ubuntu"
+env.hosts = ["54.237.48.16", "18.234.145.133"]
 
 
 def do_clean(number=0):
@@ -18,16 +17,10 @@ def do_clean(number=0):
     number: the number of archives to keep while
     deleting the rest
     """
+    number = int(number)
+    local_dir = "versions"
+    run(f"find {local_dir} -type f -name '*.tar.gz' | sort -nr | tail -n +{int(number + 1)} | xargs rm -f")
 
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+      # Remote versions folder cleanup (on both web servers)
+    remote_dir = "/data/web_static/releases"
+    sudo(f"find {remote_dir} -type f -name '*.tar.gz' | sort -nr | tail -n +{int(number + 1)} | xargs rm -f")
