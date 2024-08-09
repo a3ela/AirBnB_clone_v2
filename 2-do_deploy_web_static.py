09fc2e536a597960ccae8ead234b332afc768dfb
a3ela <abelsintayehu626@gmail.com>
2024-08-09 12:09:15 +0300

    refined

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,46 +12,52 @@ import os
 env.hosts = ['54.237.48.16', '18.234.145.133']
 env.user = "ubuntu"
 
-def do_deploy(archive_path):
-    """""deploy you codebase"""
-    t_str = time.strftime('%Y%m%d%H%M%S')
-    f_name = archive_path.split('/')[-1]
-    f_noext = f_name.split('.')[0]
+def do_pack():
+    """Function to compress files in an archive"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
 
+
+def do_deploy(archive_path):
+    """Function to distribute an archive to a server"""
     if not os.path.exists(archive_path):
         return False
-    try:
-        # upload the archive to tmp of the wen server
-        put(archive_path, '/tmp/')
-
-        # create a file with the time stamp
-        run('sudo mkdir -p /data/web_static/releases/{}/'.
-            format(f_noext))
-
-        # uncomprestion
-        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-            format(f_name, f_noext))
-
-        # delete the archive
-        run('sudo rm /tmp/{}'.format(f_name))
-
-        # move contents into web_static
-        run('sudo mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}'.format(f_noext, f_noext))
-
-        # remove irrelevant web_static dir
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'.
-            format(f_noext))
-
-        # delete the symobolic link from the web server
-        run('sudo rm -rf /data/web_static/current')
-
-        # create a new stybolic link
-        run('sudo ln -s /data/web_static/releases/{}/ \
-                /data/web_static/current'.format(f_noext))
-
-        print("New version deployed!")
-    except BaseException:
+    rex = r'^versions/(\S+).tgz'
+    match = re.search(rex, archive_path)
+    filename = match.group(1)
+    res = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if res.failed:
         return False
-
+    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if res.failed:
+        return False
+    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("rm /tmp/{}.tgz".format(filename))
+    if res.failed:
+        return False
+    res = run("mv /data/web_static/releases/{}"
+              "/web_static/* /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("rm -rf /data/web_static/releases/{}/web_static"
+              .format(filename))
+    if res.failed:
+        return False
+    res = run("rm -rf /data/web_static/current")
+    if res.failed:
+        return False
+    res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+              .format(filename))
+    if res.failed:
+        return False
+    print('New version deployed!')
     return True
